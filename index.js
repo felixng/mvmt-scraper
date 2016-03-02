78ab2b220533235b4df438d4546e745c4a7206a0
Felix Ng <felix.ng@euromoneyasia.com>
2016-03-02 14:30:40 +0000

    facebook api

--- index.js
+++ index.js
@@ -1,5 +1,35 @@
-var express = require('express');
-var app = express();
+var FB = require('fb');
+var accessToken = '';
+FB.api('oauth/access_token', {
+    client_id: '1584641475160139',
+    client_secret: 'd1690d44adb151149209e2912ceef21e',
+    grant_type: 'client_credentials'
+}, function (res) {
+    if(!res || res.error) {
+        console.log(!res ? 'error occurred' : res.error);
+        return;
+    }
+
+    accessToken = res.access_token;
+    
+    FB.setAccessToken(accessToken);
+    FB.api('/mvmtApp', { fields: ['id', 'name', 'about'] }, function (res) {
+      if(!res || res.error) {
+       console.log(!res ? 'error occurred' : res.error);
+       return;
+      }
+      console.log(res);
+    });
+});
+
+
+
+
+
+var CronJob = require('cron').CronJob;
+new CronJob('* * * * * *', function() {
+  console.log('You will see this message every second');
+}, null, true, 'America/Los_Angeles');
 
 app.set('port', (process.env.PORT || 5000));
 
