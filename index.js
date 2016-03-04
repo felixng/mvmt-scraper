021645113d203db5bf77bfdcab95e2c6266a5988
Felix Ng <felix.ng@euromoneyasia.com>
2016-03-04 16:27:09 +0000

    read from db and ping facebook api successfully.  Need to hanlde error from facebook api, and refactor code into smaller pieces

--- index.js
+++ index.js
@@ -3,32 +3,55 @@ var app = express();
 var FB = require('fb');
 var CronJob = require('cron').CronJob;
 var job = new CronJob({
-  cronTime: '0 0 * * * *',
-  grab: function() {
+  cronTime: '* * * * * *',
+  onTick: function() {
 	 FB.api('oauth/access_token', {
-				client_id: '1584641475160139',
-				client_secret: 'd1690d44adb151149209e2912ceef21e',
+				client_id: '1584642161826737',
+				client_secret: 'ba483f4532ef9d6ad76c4e67db9b50ce',
 				grant_type: 'client_credentials'
 			}, function (response) {
 				if(!response || response.error) {
 					console.log(!response ? 'error occurred' : response.error);
 					return;
 				}
-
 				accessToken = response.access_token;
-				
 				FB.setAccessToken(accessToken);
-				FB.api('/mvmtApp', { fields: ['id', 'name', 'about'] }, function (response) {
-				  if(!response || response.error) {
-				   console.log(!response ? 'error occurred' : response.error);
-				   return;
+				var pg = require('pg');
+
+				var conString = "postgres://postgres:01478520@localhost/mvmt";
+
+				var client = new pg.Client(conString);
+				client.connect(function(err) {
+				  if(err) {
+					return console.error('could not connect to postgres', err);
 				  }
-				  console.log(new Date());
-				  console.log(response);
+				  client.query("SELECT facebook FROM public.resources where trim(facebook) != '';", function(err, result) {
+					if(err) {
+					  return console.error('error running query', err);
+					}
+					
+					result.rows.forEach(function(entry) {
+						//ping facebook
+						var query = '/' + entry.facebook;
+						
+						FB.api(query, { fields: ['id', 'name', 'about'] }, function (response) {
+						  if(!response || response.error) {
+						   console.log(!response ? 'error occurred' : response.error);
+						   return;
+						  }
+						  console.log(new Date());
+						  console.log(response);
+						});
+					});
+					//console.log(result.rows[0].facebook);
+					client.end();
+				  });
 				});
+				
+				
 			});
   },
-  start: true,
+  start: false,
   timeZone: 'America/Los_Angeles'
 });
 job.start();
@@ -45,7 +68,6 @@ app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
   response.render('pages/index');
-  response.end(result);
 });
 
 app.listen(app.get('port'), function() {
