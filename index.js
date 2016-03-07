2df289ad5518828d381e33628308614c5e2ce008
Felix Ng <felix.ng@euromoneyasia.com>
2016-03-07 16:19:52 +0000

    refactored

--- index.js
+++ index.js
@@ -4,7 +4,61 @@ var FB = require('fb');
 var CronJob = require('cron').CronJob;
 var job = new CronJob({
   cronTime: '* * * * * *',
-  onTick: function() {
+  onTick: Task(),
+  start: false,
+  timeZone: 'America/Los_Angeles'
+});
+job.start();
+
+function getAccessToken(client_id, client_secret){
+	FB.api('oauth/access_token', {
+				client_id: '1584642161826737',
+				client_secret: 'ba483f4532ef9d6ad76c4e67db9b50ce',
+				grant_type: 'client_credentials'
+			}, function (response) {
+				if(!response || response.error) {
+					console.log(!response ? 'error occurred' : response.error);
+					return;
+				}
+				accessToken = response.access_token;
+				FB.setAccessToken(accessToken);
+			})
+}
+
+function pingAPI(){
+	var pg = require('pg');
+	var conString = "postgres://postgres:01478520@localhost/mvmt";
+	var client = new pg.Client(conString);
+	
+	client.connect(function(err) {
+	  if(err) {
+		return console.error('could not connect to postgres', err);
+	  }
+	  client.query("SELECT facebook FROM public.resources where trim(facebook) != '';", function(err, result) {
+		if(err) {
+		  return console.error('error running query', err);
+		}
+		
+		result.rows.forEach(function(entry) {
+			//ping facebook
+			var query = '/' + entry.facebook;
+			
+			FB.api(query, { fields: ['id', 'name', 'about'] }, function (response) {
+			  if(!response || response.error) {
+			   console.log(!response ? 'error occurred' : response.error);
+			   return;
+			  }
+			  console.log(new Date());
+			  console.log(response);
+			});
+		});
+		
+		client.end();
+	  });
+	});
+}
+
+function Task(){
 	 FB.api('oauth/access_token', {
 				client_id: '1584642161826737',
 				client_secret: 'ba483f4532ef9d6ad76c4e67db9b50ce',
@@ -16,47 +70,10 @@ var job = new CronJob({
 				}
 				accessToken = response.access_token;
 				FB.setAccessToken(accessToken);
-				var pg = require('pg');
 
-				var conString = "postgres://postgres:01478520@localhost/mvmt";
-
-				var client = new pg.Client(conString);
-				client.connect(function(err) {
-				  if(err) {
-					return console.error('could not connect to postgres', err);
-				  }
-				  client.query("SELECT facebook FROM public.resources where trim(facebook) != '';", function(err, result) {
-					if(err) {
-					  return console.error('error running query', err);
-					}
-					
-					result.rows.forEach(function(entry) {
-						//ping facebook
-						var query = '/' + entry.facebook;
-						
-						FB.api(query, { fields: ['id', 'name', 'about'] }, function (response) {
-						  if(!response || response.error) {
-						   console.log(!response ? 'error occurred' : response.error);
-						   return;
-						  }
-						  console.log(new Date());
-						  console.log(response);
-						});
-					});
-					//console.log(result.rows[0].facebook);
-					client.end();
-				  });
-				});
-				
-				
+				pingAPI();
 			});
-  },
-  start: false,
-  timeZone: 'America/Los_Angeles'
-});
-job.start();
-
-
+}
 
 app.set('port', (process.env.PORT || 5000));
 
