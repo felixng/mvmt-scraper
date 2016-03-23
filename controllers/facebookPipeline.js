a6d1b03e5a11bd2faeef3a42b339530d309a8ed8
Felix Ng <felix.ng@euromoneyasia.com>
2016-03-23 16:01:21 +0000

    not working

--- controllers/facebookPipeline.js
+++ controllers/facebookPipeline.js
@@ -1,4 +1,7 @@
 var FB = require('fb');
+var pg = require('pg');
+var conString = "postgres://postgres:01478520@localhost/mvmt";
+
 
 var config = {
 	columns: ['id', 'name', 'about'],
@@ -40,15 +43,14 @@ module.exports = {
 } */
 
 var pingAPI = function(){
-	var pg = require('pg');
-	var conString = "postgres://postgres:01478520@localhost/mvmt";
+	
 	var client = new pg.Client(conString);
 	
 	client.connect(function(err) {
 	  if(err) {
 		return console.error('could not connect to postgres', err);
 	  }
-	  client.query("SELECT * FROM public.resources where trim(facebook) != '';", function(err, result) {
+	  client.query("SELECT id, public.resources.facebook, public.resources.desc FROM public.resources where trim(facebook) != '' and id = 42;", function(err, result) {
 		if(err) {
 		  return console.error('error running query', err);
 		}
@@ -72,10 +74,44 @@ var pingAPI = function(){
 	});
 }
 
+function updateResource (id, cols) {
+  // Setup static beginning of query
+  var query = ['UPDATE public.resources'];
+  query.push('SET');
+	console.log(cols);
+  // Create another array storing each set command
+  // and assigning a number value for parameterized query
+  var set = [];
+  Object.keys(cols).forEach(function (obj) {
+	console.log(obj);
+    //set.push(key + ' = ($' + (i + 1) + ')'); 
+  });
+  query.push(set.join(', '));
+
+  // Add the WHERE statement to look up by id
+  query.push('WHERE public.resources.id = ' + id );
+
+  // Return a complete query string
+  return query.join(' ');
+}
+
 var checkAndUpdate = function(entry, response){
 	//be very defensive
 	if (response.about && response.about.length && response.about.length > entry.desc){
-		console.log("===============" + response.name + "NEED TO UPDATE===============")
+		var query = updateResource(entry.id, {['desc']});
+		console.log(query);
+		
+		var client = new pg.Client(conString);
+		client.connect(function(err) {
+			client.query(query, entry, function(err, result) {
+				if(err) {
+				  return console.error('error running query', err);
+				};
+				console.log("DONE");
+			});
+		});	
+		
+		console.log("===============" + response.name + "NEED TO UPDATE " + response.about.length + "===============")
 		console.log(response.about);
 		console.log(entry.desc);
 	}
